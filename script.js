cac696f0acc764460b3ab98e1405bc3b88734c44
Vanesa Edo <vanesa.en@gmail.com>
2024-02-12 20:22:21 +0100

    Solución + Zona Premium ok

--- script.js
+++ script.js
@@ -3,11 +3,21 @@
 const chauchat = () => {
     for (let i = 1; i < 8; i++){
         const cargador = "pium!";
-        console.log(cargador);
+        let numeroAlAzar = Math.floor(Math.random() * 100); // Un número entre 1 y 100 al azar
+        
+        if (numeroAlAzar > 80) {
+            console.log(cargador);            
+            } else {
+            console.log("Illo, me he quedao pillá!");
+            }  
+      if (i%3 === 0){
+                console.log('');
+              }
     }
 };
 
 chauchat();
 
 
+ 
 
