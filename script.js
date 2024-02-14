fff303be057532eb9ec452f65d3492f1414cf46b
Vanesa Edo <vanesa.en@gmail.com>
2024-02-14 10:22:13 +0100

    Añadida solución de clase

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Chauchaut
 
-const chauchat = () => {
+/*const chauchat = () => {
     for (let i = 1; i < 8; i++){
         const cargador = "pium!";
         let numeroAlAzar = Math.floor(Math.random() * 100); // Un número entre 1 y 100 al azar
@@ -17,6 +17,33 @@ const chauchat = () => {
 };
 
 chauchat();
+*/
+
+//Solución aportada en clase
+
+//Chauchau babyyy
+
+const chauchat = () => {
+    for (let i = 1; i < 8; i++) {
+        const cargador = "pium!";
+        let numeroAlAzar = Math.floor(Math.random() * 100); // Un número entre 1 y 100 al azar
+        if (numeroAlAzar > 10) {
+            console.log(cargador);
+        } else {
+            console.log("Illo, me he quedao pillá!");
+        }
+        if (i % 3 === 0) {
+            console.log('');
+        }
+    }
+};
+chauchat();
+
+
+
+
+
+
 
 
  
